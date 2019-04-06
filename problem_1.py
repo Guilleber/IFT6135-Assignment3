8100784942624266ee1d343773f3a89026d9b21a
LE BERRE Guillaume <guillaume.le-berre@loria.fr>
2019-04-06 14:14:15 -0400

    Wasserstein loss problem 1

--- problem_1.py
+++ problem_1.py
@@ -27,6 +27,9 @@ def train(metric, dist1, dist2, use_cuda=False):
     if use_cuda:
         D.cuda()
     optimizer = SGD(D.parameters(), lr=0.001)
+    if metric == 'WD':
+        dist_u = samplers.distribution2(batch_size=256)
+        lambda_ = 10
     
     for it in range(10000):
         optimizer.zero_grad()
@@ -40,6 +43,15 @@ def train(metric, dist1, dist2, use_cuda=False):
         
         if metric == 'JSD':
             loss = - torch.mean(torch.log(y_1)) - torch.mean(torch.log(1 - y_2))
+        if metric == 'WD':
+            a = torch.from_numpy(next(dist_u)).float()
+            if use_cuda:
+                a = a.cuda()
+            x_z = a*x_1 + (1 - a)*x_2
+            x_z.requires_grad = True
+            y_z = D(x_z)
+            grad_z = torch.autograd.grad(y_z, x_z, grad_outputs=torch.ones_like(y_z).cuda() if use_cuda else torch.ones_like(y_z), create_graph=True, retain_graph=True)[0]
+            loss = torch.mean(y_1) - torch.mean(y_2) + lambda_*torch.mean(torch.norm(grad_z - 1, p=2, dim=1))
             
         loss.backward()
         optimizer.step()
@@ -57,6 +69,8 @@ def estimate(metric, dist1, dist2, D, use_cuda=False):
     
     if metric == 'JSD':
         return np.log(2) + (0.5*torch.mean(torch.log(y_1)) + 0.5*torch.mean(torch.log(1 - y_2))).data.cpu().numpy()
+    if metric == 'WD':
+        return (torch.mean(y_1) - torch.mean(y_2)).data.cpu().numpy()
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -67,11 +81,11 @@ if __name__ == "__main__":
     
     if args.setup == 1:
         data_points = ([],[])
-        dist1 = samplers.distribution1(0, batch_size=512)
+        dist1 = samplers.distribution1(0, batch_size=256)
         for x in np.arange(-1.0, 1.01, 0.1):
             x = np.around(x, decimals=1)
             print("x = " + str(x))
-            dist2 = samplers.distribution1(x, batch_size=512)
+            dist2 = samplers.distribution1(x, batch_size=256)
             D = train(args.metric, dist1, dist2, args.use_cuda)
             y = estimate(args.metric, dist1, dist2, D, args.use_cuda)
             data_points[0].append(x)
