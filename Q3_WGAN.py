f33ac43bc8cb028a5207c47247cdc79aef199355
William St-Arnaud <william.starnaud@gmail.com>
2019-04-21 16:22:30 -0400

    Final shape of the network

--- Q3_WGAN.py
+++ Q3_WGAN.py
@@ -18,12 +18,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-t", action="store_true", help="Flag to specify if we train the model")
 parser.add_argument("--save_path", type=str, default="q3_gan.pt")
 parser.add_argument("--load_path", type=str, default="q3_gan.pt")
-parser.add_argument("--batch_size", type=int, default=32, help="Size of the mini-batches")
+parser.add_argument("--batch_size", type=int, default=128, help="Size of the mini-batches")
 parser.add_argument("--dimz", type=int, default=100, help="Dimension of the latent variables")
 parser.add_argument("--data_dir", type=str, default="svhn.mat", help="SVHN dataset location")
+parser.add_argument("--nb_epochs", type=int, default=20, help = "The number of epochs for training")
 parser.add_argument("--lam",  type=int, default=10, help="Lambda coefficient for the regularizer in"
                                                             "in the WGAN-GP loss")
-parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate for the optimzer")
+parser.add_argument("--lr", type=float, default=2e-4, help="Learning rate for the optimzer")
 parser.add_argument("--update_ratio", type=int, default=5, help="The number of updates to  the discriminator"
                                                                  "before one update to the generator")
 parser.add_argument("--eps", type=float, default=1e-1, help="Perturbation value to the latent when evaluating")
@@ -43,24 +44,23 @@ class D(nn.Module):
 
         self.convs = nn.Sequential(
             # layer1
-            nn.Conv2d(3, 128, 4, padding=1, stride=2),
-            nn.LeakyReLU(2e-2),
+            nn.Conv2d(3, 128, 5, padding=2, stride=2),
+            nn.LeakyReLU(0.2),
 
             # layer2
-            nn.Conv2d(128, 256, 4, padding=1, stride=2),
-            nn.LeakyReLU(2e-2),
+            nn.Conv2d(128, 256, 5, padding=2, stride=2),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
 
             # layer3
-            nn.Conv2d(256, 512, 4, padding=1, stride=2),
-            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(256, 512, 5, padding=2, stride=2),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
 
             # layer 4
             View(-1, 4*4*512),
-            nn.Linear(4 * 4 * 512, self.dimz),
-            nn.LeakyReLU(2e-2),
-
-            # layer 5
-            nn.Linear(self.dimz, 1),
+            nn.Linear(4 * 4 * 512, 1),
+            nn.BatchNorm1d(1),  # may have to remove
             nn.Sigmoid(),
         )
 
@@ -79,19 +79,22 @@ class G(nn.Module):
         self.deconvs = nn.Sequential(
             # layer 1
             nn.Linear(self.dimz, 4 * 4 * 512),
-            nn.LeakyReLU(2e-2),
+            nn.BatchNorm1d(4*4*512),
+            nn.ReLU(),
             View(-1, 512, 4, 4),
 
             # layer2
-            nn.ConvTranspose2d(512, 256, 4, padding=1, stride=2),
-            nn.LeakyReLU(2e-2),
+            nn.ConvTranspose2d(512, 256, 5, padding=2, stride=2, output_padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
 
             # layer 3
-            nn.ConvTranspose2d(256, 128, 4, padding=1, stride=2),
-            nn.LeakyReLU(2e-2),
+            nn.ConvTranspose2d(256, 128, 5, padding=2, stride=2, output_padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
 
             # layer 4
-            nn.ConvTranspose2d(128, 3, 4, padding=1, stride=2),
+            nn.ConvTranspose2d(128, 3, 5, padding=2, stride=2, output_padding=1),
             nn.Tanh()
         )
 
@@ -109,7 +112,7 @@ def wgan_gp_loss(real, fake, grad, lam):
     :param: The lambda factor applied for regularization
     :return: The WGAN-GP loss over all elements in the mini-batch. Size is [batch_size,]
     """
-    return real - fake - lam * (torch.norm(grad, dim=1) - 1)**2
+    return fake - real + lam * (torch.norm(grad, dim=1) - 1)**2
 
 
 def train_model(g, d, train, valid, save_path):
@@ -125,44 +128,40 @@ def train_model(g, d, train, valid, save_path):
     g_optim = optim.Adam(g.parameters(), lr=args.lr, betas=(0, 0.9))
     d_optim = optim.Adam(d.parameters(), lr=args.lr, betas=(0, 0.9))
 
-    # variable to determine whose turn it is to update its parameters
-    turn = 0
+    # print PIL image
+    display = transforms.ToPILImage()
 
-    for epoch in range(20):
+    for epoch in range(args.nb_epochs):
         for i, (batch, label) in enumerate(train):
             # put batch on device
             batch = batch.to(args.device)
 
-            if turn // args.update_ratio == 0:
+            # obtain the discriminator output on real data
+            real_prob = d(batch)
 
-                # obtain the discriminator output on real data
-                real_prob = d(batch)
+            # obtain the discriminator output on the fake data
+            z = torch.randn(batch.size()[0], g.dimz, device=args.device)
+            fake = g(z)
+            fake_prob = d(fake)
 
-                # obtain the discriminator output on the fake data
-                z = torch.randn(batch.size()[0], g.dimz, device=args.device)
-                fake = g(z)
-                fake_prob = d(fake)
+            # obtain the gradient term of the WGAN-GP loss
+            a = torch.rand_like(batch, device=args.device)
+            conv = a * batch + (1 - a) * fake
+            d_conv = d(conv)
+            grad = autograd.grad(d_conv, conv, torch.ones_like(d_conv).to(args.device),
+                                 retain_graph=True, create_graph=True, only_inputs=True)[0]
 
-                # obtain the gradient term of the WGAN-GP loss
-                a = torch.rand_like(batch, device=args.device)
-                conv = a * batch + (1 - a) * fake
-                d_conv = d(conv)
-                grad = autograd.grad(d_conv, conv, torch.ones_like(d_conv).to(args.device),
-                                     retain_graph=True, create_graph=True, only_inputs=True)[0]
+            # compute the WGAN-GP loss
+            loss = wgan_gp_loss(real_prob, fake_prob, grad.view(-1, 3 * 32 * 32), args.lam).mean()
 
-                # compute the WGAN-GP loss
-                loss = wgan_gp_loss(real_prob, fake_prob, grad.view(-1, 3*32*32), args.lam).mean()
+            # minimize the loss
+            autograd.backward([loss])
 
-                # minimize the loss
-                autograd.backward([-loss])
+            # update the parameters
+            d_optim.step()
+            d_optim.zero_grad()
 
-                # update the parameters
-                d_optim.step()
-                d_optim.zero_grad()
-
-                turn += 1
-
-            else:
+            if i % args.update_ratio == 0. and i > 0:
                 # update the generator
                 z = torch.randn(batch.size()[0], g.dimz, device=args.device)
                 fake = g(z)
@@ -171,10 +170,9 @@ def train_model(g, d, train, valid, save_path):
                 loss.backward()
                 g_optim.step()
                 g_optim.zero_grad()
-                turn = 0
 
-        # compute the loss for the validation set
         with torch.no_grad():
+            # After training for an epoch, output validation loss
             valid_loss = torch.zeros(1)
             nb_batches = 0
             for i, (batch, label) in enumerate(valid):
@@ -183,8 +181,9 @@ def train_model(g, d, train, valid, save_path):
                 real_prob = d(batch)
                 z = torch.randn(batch.size()[0], g.dimz, device=args.device)
                 fake = g(z)
+                display(((fake[0] + 1.) * 255.).to(device='cpu', copy=True)).show()
                 fake_prob = d(fake)
-                a = torch.randn_like(batch, device=args.device)
+                a = torch.rand_like(batch, device=args.device)
 
                 with torch.enable_grad():
                     conv = a * batch + (1. - a) * fake
@@ -192,10 +191,10 @@ def train_model(g, d, train, valid, save_path):
                     d_conv = d(conv)
                     grad = autograd.grad(d_conv, conv, torch.ones_like(d_conv).to(args.device),
                                          retain_graph=False, create_graph=True, only_inputs=True)[0]
-                batch_loss = wgan_gp_loss(real_prob, fake_prob, grad.view(-1, 3*32*32), args.lam)
+                batch_loss = wgan_gp_loss(real_prob, fake_prob, grad.view(-1, 3 * 32 * 32), args.lam)
                 valid_loss += batch_loss.mean()
             valid_loss /= nb_batches
-            print("After epoch {} the validation loss is: ".format(epoch+1), valid_loss.item())
+            print("After epoch {} the validation loss is: ".format(epoch + 1), valid_loss.item())
 
     # save the model to be used later
     torch.save(g.state_dict(), save_path)
@@ -209,8 +208,8 @@ def evaluation(model):
     """
     with torch.no_grad():
         transf = transforms.ToPILImage()
-        z = torch.rand(model.batch_size, model.dimz, device=args.device)
-        samples = model.dec(z)
+        z = torch.randn(model.batch_size, model.dimz, device=args.device)
+        samples = model.deconvs(z)
 
         if not os.path.isdir(args.sample_dir):
             os.mkdir(args.sample_dir)
@@ -226,7 +225,7 @@ def evaluation(model):
 
         # perturb the z and get samples
         z_ = z + args.eps
-        samples = model.dec(z_)
+        samples = model.deconvs(z_)
 
         perturb_dir = os.path.join(args.sample_dir, "perturbed_samples")
         if not os.path.isdir(perturb_dir):
@@ -244,12 +243,12 @@ def evaluation(model):
         # interpolate between two z's and generate samples. Save them
         for a in range(11):
             z_a = a / 10. * z[0:1] + (1. - a/10.) * z[1:2]
-            gz_a = model.dec(z_a)[0]
+            gz_a = model.deconvs(z_a)[0]
             im = transf(gz_a.to(device='cpu'))
             im.save(os.path.join(int_dir, "i1_img_{}.jpeg".format(a)))
 
         # interpolate the result of the two g(z) values
-        g_z = model.dec(z[0:2])
+        g_z = model.deconvs(z[0:2])
         for a in range(11):
             x_a = a / 10. * g_z[0] + (1. - a / 10.) * g_z[1]
             im = transf(x_a.to(device='cpu'))
